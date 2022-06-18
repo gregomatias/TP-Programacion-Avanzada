d101b8d3dfd4bab321663955b6f4d36ae153d135
matiasgrego <matiasgrego@gmail.com>
2022-06-18 16:01:01 -0300

    incluyo menu principal- retiro:valida multiplo de 20

--- src/App.java
+++ src/App.java
@@ -11,6 +11,7 @@ public class App {
         Scanner scanner = new Scanner(System.in);
         int intentosDeLogueo = 3;
         Usuario usuarioLogueado = null;
+        boolean desloguearse = false;
 
         /* ####Alta Usuario BD#### */
         CajeroAutomatico ATM = new CajeroAutomatico();
@@ -21,7 +22,7 @@ public class App {
 
         System.out.println("Bienvenido al Cajero Automatico ATM\n");
 
-        //Valido 3 intentos de logueo del usuario
+        // Valido 3 intentos de logueo del usuario
         while (intentosDeLogueo > 0 && usuarioLogueado == null) {
             intentosDeLogueo--;
             System.out.println("Ingrese su numero de cuenta Bancaria\n");
@@ -34,17 +35,40 @@ public class App {
 
         }
 
-         //Si el usuario esta logueado inicia el menu:
-        if (!(usuarioLogueado==null)) {
-            System.out.println("1-Depósito 2-Extracción 3-Consulta de Saldo 4-Deslogueo");
+        // Si el usuario esta logueado inicia el menu:
+        if (!(usuarioLogueado == null)) {
 
-            
-        }else{
+            while (desloguearse == false) {
+
+                System.out.println("1-Depósito 2-Extracción 3-Consulta de Saldo 4-Deslogueo\n");
+                int menuOpciones = scanner.nextInt();
+
+                switch (menuOpciones) {
+                    case 1:
+                        System.out.println("Ingrese el monto a depositar\n");
+                        double montoDeposito = scanner.nextDouble();
+                        System.out.println(ATM.depositarFondos(montoDeposito, usuarioLogueado));
+                        break;
+                    case 2:
+                        System.out.println("Ingrese el monto a extraer\n");
+                        double montoRetiro = scanner.nextDouble();
+                        System.out.println(ATM.retirarEfectivo(montoRetiro, usuarioLogueado));
+                        break;
+                    case 3:
+                        System.out
+                                .println("El saldo de su cuenta es: " + usuarioLogueado.getCuentaBancaria().getSaldo()+"\n");
+                        break;
+                    case 4:
+                        System.out.println("Por favor, retire su tarjeta.\n Muchas gracias por operar con cajeros ATM\n");
+                        desloguearse = true;
+                        break;
+                }
+
+            }
+
+        } else {
             System.out.println("Ha superado el numero de intentos, por su seguridad retuvimos su tarjeta.");
         }
-
-
-
 
         scanner.close();
     }
--- src/cajero/CajeroAutomatico.java
+++ src/cajero/CajeroAutomatico.java
@@ -15,15 +15,22 @@ public class CajeroAutomatico {
 
         if (this.efectivoDelCajero >= montoRetiro) {
 
-            if (usuario.getCuentaBancaria().getSaldo() > montoRetiro) {
+            if (usuario.getCuentaBancaria().getSaldo() >= montoRetiro) {
+
+              double  resto =montoRetiro%20;
+              System.out.println("Resto: "+resto);
+                if(resto>0){
+                    
+                    return "El monto a extraer debe ser multiplo de 20\n";
+                }
 
                 usuario.getCuentaBancaria().setSaldo(usuario.getCuentaBancaria().getSaldo() - montoRetiro);
-                return "Retiro de Dinero exitoso, su saldo actual es: "+usuario.getCuentaBancaria().getSaldo();
+                return "Retiro de Dinero exitoso, su saldo actual es: " + usuario.getCuentaBancaria().getSaldo() + "\n";
             } else {
-                return "El monto ingresado supera el saldo de su cuenta";
+                return "El monto ingresado supera el saldo de su cuenta\n";
             }
         } else {
-            return "Disculpe, el cajero no tiene fondos para procesar dicha operacion, intente con un monto menor";
+            return "Disculpe, el cajero no tiene fondos para procesar dicha operacion, intente con un monto menor\n";
         }
 
     }
